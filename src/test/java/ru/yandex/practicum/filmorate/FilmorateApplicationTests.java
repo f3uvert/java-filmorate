24cf1679c7e633a2afa82a6a4d27b5b6cffc5624
f3uvert <geradaividov@gmail.com>
2025-09-29 13:40:51 +0300

    fix issues

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,15 +1,15 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
+//import ru.yandex.practicum.filmorate.model.Film;
+//import ru.yandex.practicum.filmorate.model.User;
 
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+//import java.time.LocalDate;
+//import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class FilmorateApplicationTests {
