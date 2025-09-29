fa5a9509d2718217ff2f71fd3cce348218d48467
f3uvert <geradaividov@gmail.com>
2025-09-29 13:48:34 +0300

    fix issues

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -61,20 +61,4 @@ CREATE TABLE IF NOT EXISTS likes (
     PRIMARY KEY (film_id, user_id),
     FOREIGN KEY (film_id) REFERENCES films(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-
-    MERGE INTO mpa_ratings (id, name, description) VALUES
-    (1, 'G', 'Нет возрастных ограничений'),
-    (2, 'PG', 'Рекомендуется присутствие родителей'),
-    (3, 'PG-13', 'Детям до 13 лет просмотр не желателен'),
-    (4, 'R', 'Лицам до 17 лет обязательно присутствие взрослого'),
-    (5, 'NC-17', 'Лицам до 18 лет просмотр запрещен');
-
-
-    MERGE INTO genres (id, name) VALUES
-    (1, 'Комедия'),
-    (2, 'Драма'),
-    (3, 'Мультфильм'),
-    (4, 'Триллер'),
-    (5, 'Документальный'),
-    (6, 'Боевик');
 );
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,19 +1,19 @@
 package ru.yandex.practicum.filmorate;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//import ru.yandex.practicum.filmorate.model.Film;
-//import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
 
-//import java.time.LocalDate;
-//import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
-//import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class FilmorateApplicationTests {
-    /*
+
     @Test
     void testFilmValidation() {
         Film film = new Film();
@@ -177,5 +177,5 @@ class FilmorateApplicationTests {
         assertThrows(NotFoundException.class, () -> validateFilm(film));
     }
 
-     */
+
 }
