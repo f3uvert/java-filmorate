349cdc661c916d9ec673c8b722fa430974ea540b
f3uvert <geradaividov@gmail.com>
2025-10-01 01:04:14 +0300

    fix issues

--- src/main/java/ru/yandex/practicum/filmorate/storage/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaStorage.java
@@ -1,10 +1,14 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface MpaStorage {
+
     List<Film.Mpa> getAllMpa();
+
     Optional<Film.Mpa> getMpaById(int id);
+
 }
